7bc0c801c0b566b3f5e5b196d8f46f26093b78fa
Andres Campuzano <campuxano88@gmail.com>
2020-06-12 14:49:00 -0500

    Rendering info from API, also search bar working properly.

--- src/components/CardHome.jsx
+++ src/components/CardHome.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const CardHome = ({ key, name }) => {
+const CardHome = ({ flag, name, population, region, capital }) => {
   // console.log('from CardHome: ', props);
 
   return (
     <>
-      <li key={key}>{name}</li>
+      <li key={name}>
+        <img src={flag} alt={name} />
+        <h2>{name}</h2>
+        <p>{population}</p>
+        <p>{region}</p>
+        <p>{capital}</p>
+      </li>
     </>
   );
 };
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,23 +1,48 @@
 import React, { useState, useEffect } from 'react';
-// import CardHome from '../components/CardHome';
+import CardHome from '../components/CardHome';
 
 const Home = () => {
-  const [data, setData] = useState({
-    info: ''
-  });
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+
+  console.log('original data: ', data);
+  console.log('filteredData: ', filteredData);
 
   useEffect(() => {
     fetch('https://restcountries.eu/rest/v2/all')
       .then(response => response.json())
-      .then(response => setData({ info: response }));
+      .then(response => {
+        setData([...response]);
+        setFilteredData([...response]);
+      })
+      .catch(e => console.log(e));
   }, []);
+
+  const handleChangeInput = e => {
+    const string = e.target.value.toLowerCase();
+    const stringToUppercase = string.replace(/\b\w/g, l => l.toUpperCase());
+    setFilteredData(data.filter(x => x.name.includes(stringToUppercase)));
+  };
+
+  // const convertToArray = Array.from(data.info);
+  // console.log(convertToArray);
 
   return (
     <>
       <p>This is a test from Home</p>
+      <input type='search' onChange={handleChangeInput} id='search' />
       <ul>
-        {Object.keys(data.info).map(x => {
-          return <li key={x.index}>{x.name}</li>;
+        {filteredData.map(({ flag, name, population, region, capital }) => {
+          return (
+            <CardHome
+              flag={flag}
+              name={name}
+              key={name}
+              population={population}
+              region={region}
+              capital={capital}
+            />
+          );
         })}
       </ul>
     </>
