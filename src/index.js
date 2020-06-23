22a7a1fd5a7dd9eb7d0556c2d5327c80565445b0
Andres Campuzano <campuxano88@gmail.com>
2020-06-23 14:03:12 -0500

    PWA, working in local.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './routes/App';
+import * as serviceWorker from './serviceWorker';
 
-// import { Provider } from 'react-redux';
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-ReactDOM.render(<App />, document.getElementById('app'));
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
